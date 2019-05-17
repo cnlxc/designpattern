de66ddabf40bc84b0b1e11d82ee74a3ab1bb3cd1
cnlxc <821382992@qq.com>
2019-05-17 12:37:55 +0800

    demo of observer

---
+++ src/main/java/observer/SportSubject.java
@@ -0,0 +1,11 @@
+package observer;
+
+/**
+ * Created by 82138 on 2019/4/27.
+ */
+public class SportSubject extends BaseSubject{
+    public SportSubject(String name){
+        super();//只是习惯
+        super.name = name;
+    }
+}
